6ddba12f84c7e6fff2fa506c639f0336376ee486
simonguo <simonguo.2009@gmail.com>
2016-12-04 10:35:43 +0800

    Update index page

--- src/fixtures/DocFooter.js
+++ src/fixtures/DocFooter.js
@@ -11,13 +11,11 @@ const DocFooter = React.createClass({
 
 
                     Powered by<a href="https://www.hypers.com" target="_blank">Hypers FE</a>
-                    <a href="https://gitter.im/rsuite/rsuite?utm_source=badge&amp;utm_medium=badge&amp;utm_campaign=pr-badge&amp;utm_content=badge">
-                    <img src="https://camo.githubusercontent.com/9e6bb52fcbafda44267c988c33a205c7938d8601/68747470733a2f2f6261646765732e6769747465722e696d2f7273756974652f7273756974652e737667" alt="Join the chat at https://gitter.im/rsuite/rsuite" data-canonical-src="https://badges.gitter.im/rsuite/rsuite.svg" />
+                    <a href="https://gitter.im/rsuite/rsuite?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge">
+                        <img src="https://badges.gitter.im/rsuite/rsuite.svg" alt="Join the chat at https://gitter.im/rsuite/rsuite"  />
                     </a>
                     |
-
                     <a href="https://github.com/rsuite/rsuite" target="_blank"><i className="fa fa-github"></i></a>
-
 
                 </div>
            </div>
