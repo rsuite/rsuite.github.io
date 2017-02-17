cfaa26ce156817b5e5b550b135726c2130980414
simonguo <simonguo.2009@gmail.com>
2017-02-17 19:28:51 +0800

    build

--- src/fixtures/DocHeader.js
+++ src/fixtures/DocHeader.js
@@ -85,7 +85,7 @@ const DocHeader = React.createClass({
                             {links}
                         </Nav>
                         <Nav pullRight>
-                            <Nav.Item href="https://github.com/rsuite/rsuite">GitHub</Nav.Item>
+                            <Nav.Item href="https://github.com/rsuite/rsuite"><i className="fa fa-github"></i> GitHub</Nav.Item>
                         </Nav>
                     </Navbar.Collapse>
                 </div>
