d6304522b2f40ed753994b6aec5eb4c3371dca45
simonguo <simonguo.2009@gmail.com>
2017-03-18 15:48:24 +0800

    Support IE9

--- src/index.js
+++ src/index.js
@@ -13,30 +13,36 @@ import 'codemirror/theme/base16-dark.css';
 
 require('./hypers-hire');
 
-import App from  './components/App';
+import App from './components/App';
 
 // Pages
-import PageIndex from  './pages/PageIndex';
-import PageGettingStarted from  './pages/PageGettingStarted';
+import PageIndex from './pages/PageIndex';
+import PageGettingStarted from './pages/PageGettingStarted';
 import PageComponents from './pages/PageComponents';
 import PageComponentsDoc from './pages/PageComponentsDoc';
 import PageExamples from './pages/PageExamples';
 
+import ready from './ready';
 
-render((
+const mountApp = (
     <Router history={hashHistory} >
 
         <Route path="/" component={App}>
-            <IndexRoute component={PageIndex}/>
+            <IndexRoute component={PageIndex} />
             <Route path="getting-started" component={PageGettingStarted} />
             <Route path="examples" component={PageExamples} />
 
             <Route path="components" component={PageComponents}>
-                <Route path=":name"  component={PageComponentsDoc} />
+                <Route path=":name" component={PageComponentsDoc} />
                 <IndexRedirect to="buttons" />
             </Route>
         </Route>
 
     </Router>
-), document.getElementById('root'));
+);
+
+ready((values) => {
+    render(mountApp, document.getElementById('root'));
+});
+
 
---
+++ src/ready.js
@@ -0,0 +1,49 @@
+
+function loadJsFile(url, callback) {
+    var s = document.createElement('script'),
+        head = document.getElementsByTagName('head')[0];
+    s.type = 'text/javascript';
+    //注意服务器返回的JS的编码需与这里的设置一致
+    s.charset = 'utf-8';
+    s.src = url;
+    s.onload = () => {
+        callback && callback();
+    };
+    head.insertBefore(s, head.firstChild);
+    return s;
+}
+
+
+const isIE = !!navigator.userAgent.match(/MSIE/);
+const filter = [];
+
+/**
+ * polyfill & es6-shim
+ */
+filter.push(new Promise((resolve, reject) => {
+    if (!isIE) {
+        return resolve();
+    }
+
+    let count = 0;
+    const files = [
+        'http://cdn.bootcss.com/es6-shim/0.35.1/es6-shim.min.js',
+        'http://cdn.bootcss.com/es6-shim/0.35.1/es6-sham.min.js'
+    ];
+    //异步加载CSS文件
+    files.map((file) => {
+        loadJsFile(file, () => {
+            count++;
+            if (count === files.length) {
+                resolve();
+            }
+        });
+    });
+
+}));
+
+export default function ready(callback) {
+    Promise.all(filter).then(values => {
+        callback(values);
+    });
+}
