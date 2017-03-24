bb1d470a97504be2d8d04e0983fe45340a05bd0c
Godfery <hiyangguo@qq.com>
2017-03-24 17:03:29 +0800

    update:add clipboard componet && fixed icon error

--- src/components/iconFont/IconList.js
+++ src/components/iconFont/IconList.js
@@ -1,18 +1,9 @@
 import React from 'react';
-import { FormControl, IconFont } from 'rsuite';
+import { FormControl } from 'rsuite';
+import NotificationSystem  from 'react-notification-system';
+import IconItem from './IconItem';
 
 const ALL_ICONS = JSON.parse(require('fs').readFileSync(__dirname + '/icons.json', 'utf8'));
-
-const IconItem = React.createClass({
-    render(){
-        return (
-            <div className="icon-item col-md-3 col-sm-4">
-                <IconFont {...this.props} className="icon-content"/>
-                <p className="icon-name-text">{this.props.icon}</p>
-            </div>
-        );
-    }
-});
 
 const NoneDom = () => <div className="col-md-12">无</div>;
 
@@ -29,20 +20,38 @@ export default React.createClass({
             icons
         });
     },
+    handleCopy(text, result){
+        const message = result ? '复制成功' : '复制失败，浏览器不支持此功能';
+        this._notificationSystem.addNotification({
+            message,
+            autoDismiss: 1.5,
+            position: 'tc',
+            level: 'success'
+        });
+    },
     renderIcon(icons, classPrefix = ''){
-        return icons.map((icon) => <IconItem icon={icon} key={icon} classPrefix={classPrefix}/>);
+        return icons.map(
+            (icon) => {
+                return <IconItem
+                    icon={icon}
+                    key={icon}
+                    classPrefix={classPrefix}
+                    handleCopy={this.handleCopy}/>;
+            }
+        );
     },
     render(){
         const { icons } = this.state;
         return (
             <div className="icon-list-wrap">
                 <FormControl type='text'
-                             placeholder="输入英文关键字进行搜索，比如 play"
+                             placeholder="输入英文关键字进行搜索，比如 play。点击图标，复制图标名称。"
                              onChange={this.handleSearch}/>
                 <hr/>
                 <div className="row icon-item-list">
                     {icons.length > 0 ? this.renderIcon(icons, 'icon') : <NoneDom/>}
                 </div>
+                <NotificationSystem ref={ref => this._notificationSystem = ref}/>
             </div>
         );
     }
