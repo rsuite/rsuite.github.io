14e45d13304210d8f306fbc3a62d60bbde12f96d
simonguo <simonguo.2009@gmail.com>
2017-04-20 11:32:01 +0800

    reset hashHistory

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ import PageExamples from './pages/PageExamples';
 import ready from './ready';
 
 const mountApp = (
-    <Router history={browserHistory} >
+    <Router history={hashHistory} >
 
         <Route path="/" component={App}>
             <IndexRoute component={PageIndex} />
