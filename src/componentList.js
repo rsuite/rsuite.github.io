442f98938abc1db1be4f9b2a73355def67b4a7f8
simonguo <simonguo.2009@gmail.com>
2017-06-22 11:11:48 +0800

    Updates

--- src/componentList.js
+++ src/componentList.js
@@ -1,144 +1,144 @@
 export default [
-    {
-        icon: 'icon icon-briefcase',
-        bg: '#6292f0',
-        repoName: 'rsuite',
-        url: 'https://rsuitejs.com/#/components',
-        name: 'Basis Components',
-        intro: '基础组件',
-        src: 'https://github.com/rsuite/rsuite'
-    }, {
-        icon: 'icon icon-table',
-        bg: '#004889',
-        repoName: 'rsuite-table',
-        url: 'https://rsuitejs.com/rsuite-table',
-        name: 'RSuite Table',
-        intro: 'Table 组件，支持Tree，自定义列宽，锁定列及表头等功能',
-        src: 'https://github.com/rsuite/rsuite-table'
-    },
-    {
-        icon: 'icon icon-calendar-check-o',
-        bg: '#d3555a',
-        repoName: 'rsuite-datepicker',
-        url: 'https://rsuitejs.com/rsuite-datepicker',
-        name: 'RSuite DatePicker',
-        intro: '日历选择组件',
-        src: 'https://github.com/rsuite/rsuite-datepicker'
-    },
-    {
-        icon: 'icon icon-calendar-minus-o',
-        bg: '#a50006',
-        repoName: 'rsuite-daterangepicker',
-        url: 'https://rsuitejs.com/rsuite-daterangepicker',
-        name: 'RSuite DateRangePicker',
-        intro: '日历范围选择组件',
-        src: 'https://github.com/rsuite/rsuite-daterangepicker'
-    },
-    {
-        icon: 'icon icon-check',
-        bg: '#ff9800',
-        repoName: 'rsuite-picker',
-        url: 'https://rsuitejs.com/rsuite-picker',
-        name: 'RSuite Picker',
-        intro: '选择组件，可以替代 Select 和 Dropdown 组件',
-        src: 'https://github.com/rsuite/rsuite-picker'
-    },
-    {
-        icon: 'icon icon-bar-chart',
-        bg: '#990066',
-        repoName: 'rsuite-echarts',
-        url: 'https://rsuitejs.com/rsuite-echarts',
-        name: 'RSuite ECharts',
-        intro: 'ECharts 的 React 组件',
-        src: 'https://github.com/rsuite/rsuite-echarts'
-    },
-    {
-        icon: 'icon icon-cloud-upload',
-        bg: '#5677AC',
-        repoName: 'rsuite-uploader',
-        url: 'https://rsuitejs.com/rsuite-uploader',
-        name: 'RSuite Uploader',
-        intro: '上传文件组件',
-        src: 'https://github.com/rsuite/rsuite-uploader'
-    },
-    {
-        icon: 'icon icon-sitemap',
-        bg: '#29a7e1',
-        repoName: 'rsuite-tree',
-        url: 'https://rsuitejs.com/rsuite-tree',
-        name: 'RSuite Tree',
-        intro: '树型组件，支持选择，支持树节点拖拽',
-        src: 'https://github.com/rsuite/rsuite-tree'
-    },
-    {
-        icon: 'icon icon-sliders',
-        bg: '#00d09d',
-        repoName: 'rsuite-slider',
-        url: 'https://rsuitejs.com/rsuite-slider',
-        name: 'RSuite Slider',
-        intro: '滑动输入条，用于在数值区间选择',
-        src: 'https://github.com/rsuite/rsuite-slider'
-    },
-    {
-        icon: 'icon icon-thumb-tack',
-        bg: '#464c5b',
-        repoName: 'rsuite-affix',
-        url: 'https://rsuitejs.com/rsuite-affix',
-        name: 'RSuite Affix',
-        intro: '固钉，将页面元素钉在可视范围',
-        src: 'https://github.com/rsuite/rsuite-affix'
-    },
-    {
-        icon: 'icon icon-search',
-        bg: '#1d9d74',
-        repoName: 'rsuite-autocomplete',
-        url: 'https://rsuite.github.io/rsuite-autocomplete',
-        name: 'RSuite Autocomplete',
-        intro: '输入框自动完成功能。',
-        src: 'https://github.com/rsuite/rsuite-autocomplete'
-    },
-    {
-        icon: 'icon icon-clipboard',
-        bg: '#b94e95',
-        repoName: 'rsuite-clipboard',
-        url: 'https://rsuite.github.io/rsuite-clipboard',
-        name: 'RSuite Clipboard',
-        intro: '复制到剪切板',
-        src: 'https://github.com/rsuite/rsuite-clipboard'
-    },
-    {
-        icon: 'icon icon-check-circle-o',
-        bg: '#00bcd4',
-        repoName: 'rsuite-steps',
-        url: 'https://rsuite.github.io/rsuite-steps',
-        name: 'RSuite Steps',
-        intro: '步骤条，是一个步骤提示组件，可以在步骤繁杂的操作中引导用户',
-        src: 'https://github.com/rsuite/rsuite-steps'
-    },{
-        icon: 'icon icon-calculator',
-        bg: '#00ddff',
-        repoName: 'rsuite-inputnumber',
-        url: 'https://rsuite.github.io/rsuite-inputnumber',
-        name: 'RSuite InputNumber',
-        intro: '数字输入框，通过鼠标或键盘，输入范围内的数值',
-        src: 'https://github.com/rsuite/rsuite-inputnumber'
-    },{
-        icon: 'icon icon-bookmark',
-        bg: '#ca565e',
-        repoName: 'rsuite-tag',
-        url: 'https://rsuitejs.com/rsuite-tag',
-        name: 'RSuite Tag',
-        intro: '对不同维度进行标记和分类的小标签。',
-        src: 'https://github.com/rsuite/rsuite-tag'
-    },{
-        icon: 'icon icon-order-form',
-        bg: '#004889',
-        repoName: 'form-lib',
-        url: 'https://rsuitejs.com/form-lib',
-        name: 'form-lib',
-        intro: 'React 实现的 Form 组件',
-        src: 'https://github.com/rsuite/form-lib'
-    }
+  {
+    icon: 'icon icon-briefcase',
+    bg: '#6292f0',
+    repoName: 'rsuite',
+    url: 'https://rsuitejs.com/#/components',
+    name: 'Basis Components',
+    intro: '基础组件',
+    src: 'https://github.com/rsuite/rsuite'
+  }, {
+    icon: 'icon icon-table',
+    bg: '#004889',
+    repoName: 'rsuite-table',
+    url: 'https://rsuitejs.com/rsuite-table',
+    name: 'RSuite Table',
+    intro: 'Table 组件，支持Tree，自定义列宽，锁定列及表头等功能',
+    src: 'https://github.com/rsuite/rsuite-table'
+  },
+  {
+    icon: 'icon icon-calendar-check-o',
+    bg: '#d3555a',
+    repoName: 'rsuite-datepicker',
+    url: 'https://rsuitejs.com/rsuite-datepicker',
+    name: 'RSuite DatePicker',
+    intro: '日历选择组件',
+    src: 'https://github.com/rsuite/rsuite-datepicker'
+  },
+  {
+    icon: 'icon icon-calendar-minus-o',
+    bg: '#a50006',
+    repoName: 'rsuite-daterangepicker',
+    url: 'https://rsuitejs.com/rsuite-daterangepicker',
+    name: 'RSuite DateRangePicker',
+    intro: '日历范围选择组件',
+    src: 'https://github.com/rsuite/rsuite-daterangepicker'
+  },
+  {
+    icon: 'icon icon-check',
+    bg: '#ff9800',
+    repoName: 'rsuite-picker',
+    url: 'https://rsuitejs.com/rsuite-picker',
+    name: 'RSuite Picker',
+    intro: '选择组件，可以替代 Select 和 Dropdown 组件',
+    src: 'https://github.com/rsuite/rsuite-picker'
+  },
+  {
+    icon: 'icon icon-bar-chart',
+    bg: '#990066',
+    repoName: 'rsuite-echarts',
+    url: 'https://rsuitejs.com/rsuite-echarts',
+    name: 'RSuite ECharts',
+    intro: 'ECharts 的 React 组件',
+    src: 'https://github.com/rsuite/rsuite-echarts'
+  },
+  {
+    icon: 'icon icon-cloud-upload',
+    bg: '#5677AC',
+    repoName: 'rsuite-uploader',
+    url: 'https://rsuitejs.com/rsuite-uploader',
+    name: 'RSuite Uploader',
+    intro: '上传文件组件',
+    src: 'https://github.com/rsuite/rsuite-uploader'
+  },
+  {
+    icon: 'icon icon-sitemap',
+    bg: '#29a7e1',
+    repoName: 'rsuite-tree',
+    url: 'https://rsuitejs.com/rsuite-tree',
+    name: 'RSuite Tree',
+    intro: '树型组件，支持选择，支持树节点拖拽',
+    src: 'https://github.com/rsuite/rsuite-tree'
+  },
+  {
+    icon: 'icon icon-sliders',
+    bg: '#00d09d',
+    repoName: 'rsuite-slider',
+    url: 'https://rsuitejs.com/rsuite-slider',
+    name: 'RSuite Slider',
+    intro: '滑动输入条，用于在数值区间选择',
+    src: 'https://github.com/rsuite/rsuite-slider'
+  },
+  {
+    icon: 'icon icon-thumb-tack',
+    bg: '#464c5b',
+    repoName: 'rsuite-affix',
+    url: 'https://rsuitejs.com/rsuite-affix',
+    name: 'RSuite Affix',
+    intro: '固钉，将页面元素钉在可视范围',
+    src: 'https://github.com/rsuite/rsuite-affix'
+  },
+  {
+    icon: 'icon icon-search',
+    bg: '#1d9d74',
+    repoName: 'rsuite-autocomplete',
+    url: 'https://rsuite.github.io/rsuite-autocomplete',
+    name: 'RSuite Autocomplete',
+    intro: '输入框自动完成功能。',
+    src: 'https://github.com/rsuite/rsuite-autocomplete'
+  },
+  {
+    icon: 'icon icon-clipboard',
+    bg: '#b94e95',
+    repoName: 'rsuite-clipboard',
+    url: 'https://rsuite.github.io/rsuite-clipboard',
+    name: 'RSuite Clipboard',
+    intro: '复制到剪切板',
+    src: 'https://github.com/rsuite/rsuite-clipboard'
+  },
+  {
+    icon: 'icon icon-check-circle-o',
+    bg: '#00bcd4',
+    repoName: 'rsuite-steps',
+    url: 'https://rsuite.github.io/rsuite-steps',
+    name: 'RSuite Steps',
+    intro: '步骤条，是一个步骤提示组件，可以在步骤繁杂的操作中引导用户',
+    src: 'https://github.com/rsuite/rsuite-steps'
+  }, {
+    icon: 'icon icon-calculator',
+    bg: '#00ddff',
+    repoName: 'rsuite-inputnumber',
+    url: 'https://rsuite.github.io/rsuite-inputnumber',
+    name: 'RSuite InputNumber',
+    intro: '数字输入框，通过鼠标或键盘，输入范围内的数值',
+    src: 'https://github.com/rsuite/rsuite-inputnumber'
+  }, {
+    icon: 'icon icon-bookmark',
+    bg: '#ca565e',
+    repoName: 'rsuite-tag',
+    url: 'https://rsuitejs.com/rsuite-tag',
+    name: 'RSuite Tag',
+    intro: '对不同维度进行标记和分类的小标签。',
+    src: 'https://github.com/rsuite/rsuite-tag'
+  }, {
+    icon: 'icon icon-order-form',
+    bg: '#004889',
+    repoName: 'form-lib',
+    url: 'https://rsuitejs.com/form-lib',
+    name: 'form-lib',
+    intro: 'React 实现的 Form 组件',
+    src: 'https://github.com/rsuite/form-lib'
+  }
 
 ];
 
