dcf034f9a08a0ad3f41d672e02bb0613b7eedebf
simonguo <simonguo.2009@gmail.com>
2017-07-17 14:08:15 +0800

    Update demo

--- src/components/controls/basic.js
+++ src/components/controls/basic.js
@@ -64,11 +64,11 @@ var instance = (
 
     <FormGroup controlId='checkboxList' >
       <ControlLabel>CheckboxGroup inline</ControlLabel>
-      <CheckboxGroup name="checkboxList" inline>
-        <Checkbox checked>Item A</Checkbox>
-        <Checkbox>Item B</Checkbox>
-        <Checkbox>Item C</Checkbox>
-        <Checkbox disabled>Item D</Checkbox>
+      <CheckboxGroup name="checkboxList" value={['A', 'C']} inline>
+        <Checkbox value="A">Item A</Checkbox>
+        <Checkbox value="B">Item B</Checkbox>
+        <Checkbox value="C">Item C</Checkbox>
+        <Checkbox value="D" disabled>Item D</Checkbox>
       </CheckboxGroup>
     </FormGroup>
 
