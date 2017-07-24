801b54ccf150717aceff08923a426c7c603bae49
simonguo <simonguo.2009@gmail.com>
2017-07-24 23:42:38 +0800

    build

--- deploy.js
+++ deploy.js
@@ -2,5 +2,5 @@ const fs = require('fs-extra');
 const routes = require('./routes.js');
 const path = require('path');
 routes.forEach((route) => {
-    fs.copySync('index.html', path.join(route, 'index.html'));
+  fs.copySync('index.html', path.join(route, 'index.html'));
 });
