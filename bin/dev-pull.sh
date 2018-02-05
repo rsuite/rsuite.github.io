5ff711eadaba64e436619c25c75a76440109bd45
simonguo <simon.guo@hypers.com>
2018-02-05 14:11:44 +0800

    Update dev-pull.sh

--- bin/dev-pull.sh
+++ bin/dev-pull.sh
@@ -5,14 +5,24 @@
 nrm use hypers
 
 
+echo '-------in rsuite.github.io-------'
+rm -R -f node_modules/rsuite-datepicker
+npm install rsuite-datepicker
+echo 're-install finish'
+
 echo '-------in rsuite-------'
 cd ../rsuite
-rm -R node_modules/rsuite-selectpicker/
-rm -R node_modules/rsuite-checkpicker/
-rm -R node_modules/rsuite-cascader/
-npm install rsuite-selectpicker
-npm install rsuite-checkpicker
-npm install rsuite-cascader
+##rm -R node_modules/rsuite-selectpicker/
+##rm -R node_modules/rsuite-checkpicker/
+##rm -R node_modules/rsuite-cascader/
+rm -R -f node_modules/rsuite-datepicker
+##npm install rsuite-selectpicker
+##npm install rsuite-checkpicker
+##npm install rsuite-cascader
+npm install rsuite-datepicker
 echo 're-install finish'
+
+
+
 
 exit;
