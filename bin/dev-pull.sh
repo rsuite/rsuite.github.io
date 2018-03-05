a1c3a4e7e134f33cad01b9ec73f1acbb25bf9c3e
simonguo <simon.guo@hypers.com>
2018-03-05 17:51:21 +0800

    Update bin/dev-pull.sh

--- bin/dev-pull.sh
+++ bin/dev-pull.sh
@@ -6,14 +6,17 @@ nrm use hypers
 
 
 echo '-------in rsuite.github.io-------'
-npm install rsuite-uploader
+## npm install rsuite-uploader
 
 echo 're-install finish'
 
 echo '-------in rsuite-------'
 cd ../rsuite
 
-npm install rsuite-uploader
+npm install rsuite-selectpicker
+npm install rsuite-checkpicker
+npm install rsuite-treepicker
+npm install rsuite-checktreepicker
 echo 're-install finish'
 
 
