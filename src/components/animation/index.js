a62dfcb62fab151dd3a0b23f63e738c3ebaaa49d
simonguo <simon.guo@hypers.com>
2018-11-27 13:24:27 +0800

    Add example for animation

--- src/components/animation/index.js
+++ src/components/animation/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import { Animation, Button } from 'rsuite';
+import { Animation, Button, ButtonToolbar } from 'rsuite';
 import createComponentExample from '../createComponentExample';
 
-const { Fade, Collapse, Transition } = Animation;
+const { Fade, Collapse, Bounce, Slide, Transition } = Animation;
 
 export default createComponentExample({
   id: 'animation',
-  examples: ['fade', 'collapse', 'transition'],
+  examples: ['fade', 'collapse', 'bounce', 'slide', 'transition'],
   dependencies: {
     Button,
+    ButtonToolbar,
     Fade,
     Collapse,
+    Bounce,
+    Slide,
     Transition
   }
 });
