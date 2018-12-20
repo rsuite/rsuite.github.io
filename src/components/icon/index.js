768b29d1a5411e6193f7d5a18802baaeb9109b7c
Godfery <hiyangguo@qq.com>
2018-12-20 11:12:45 +0800

    Update icon docs for svg icon.

--- src/components/icon/index.js
+++ src/components/icon/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import { Icon, IconStack, Button } from 'rsuite';
+import { Icon, IconStack, Button, IconButton } from 'rsuite';
 import IconLogo from '../../resources/images/logo.svg';
 import * as SvgIcons from '../../fixtures/SvgIcons';
 import createComponentExample from '../createComponentExample';
 
 export default createComponentExample({
   id: 'Icon',
-  examples: ['basic', 'spin', 'rotate', 'size', 'stack', 'custom'],
+  examples: [
+    'basic',
+    'spin',
+    'rotate',
+    'size',
+    'stack',
+    'custom',
+    'custom-svg'
+  ],
   dependencies: {
     SvgIcons,
     IconLogo,
     Icon,
     IconStack,
-    Button
+    Button,
+    IconButton
   }
 });
